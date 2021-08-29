a2fae03adbe734d3b95c428ea578a9160ff2670f
konhi <hello.konhi@gmail.com>
2021-08-29 18:24:12 +0200

    🦋 pretti!

--- index.js
+++ index.js
@@ -2,41 +2,44 @@ const fetch = require("node-fetch");
 const download = require("download-git-repo");
 const fs = require("fs");
 
-const COMMUNITY_PLUGINS_URL = 'https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-plugins.json'
+const COMMUNITY_PLUGINS_URL =
+  "https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-plugins.json";
 
 async function fetchJSON(url) {
-    return fetch(url)
-        .then((response) => response.json());
+  return fetch(url).then((response) => response.json());
 }
 
-async function downloadRepo(repo, branch = 'master') {
-    const repoDestination = `downloads/${repo}`;
+async function downloadRepo(repo, branch = "master") {
+  const repoDestination = `downloads/${repo}`;
 
-    console.log('')
-    console.log(`--- ${repo} ---`)
+  console.log("");
+  console.log(`--- ${repo} ---`);
 
-    if (!fs.existsSync(repoDestination)) {
-        download(`${repo}#${branch}`, repoDestination, function (err) {
-            if (err) {
-                console.log(`⌛ Caught error: ${err}. Trying to download from main branch...`)
-                try {
-                    downloadRepo(repo, 'main');
-                    console.log(`⭐ Hurray! Repository downloaded from main branch.`)
-                } catch (error) {
-                    console.log(`❌ Couldn't download repo!`)
-                }
-            }
-        })
-        console.log('✅ Repository downloaded.')
-    } else {
-        console.log('🔄 Repository is already downloaded, skipping...')
-    }
+  if (!fs.existsSync(repoDestination)) {
+    download(`${repo}#${branch}`, repoDestination, function (err) {
+      if (err) {
+        console.log(
+          `⌛ Caught error: ${err}. Trying to download from main branch...`
+        );
+        try {
+          downloadRepo(repo, "main");
+          console.log(`⭐ Hurray! Repository downloaded from main branch.`);
+        } catch (error) {
+          console.log(`❌ Couldn't download repo!`);
+        }
+      }
+    });
+    console.log("✅ Repository downloaded.");
+  } else {
+    console.log("🔄 Repository is already downloaded, skipping...");
+  }
 }
 
-fetchJSON(COMMUNITY_PLUGINS_URL)
-    .then(plugins => plugins.map(plugin => {
-        console.log('✅ Fetched community-plugins.json')
-        downloadRepo(plugin.repo)
-        console.log('')
-        console.log('✅ All repository got downloaded.')
-    }))
+fetchJSON(COMMUNITY_PLUGINS_URL).then((plugins) =>
+  plugins.map((plugin) => {
+    console.log("✅ Fetched community-plugins.json");
+    downloadRepo(plugin.repo);
+    console.log("");
+    console.log("✅ All repository got downloaded.");
+  })
+);
